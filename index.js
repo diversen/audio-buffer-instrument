02d18d6bdf57fc291cdad629b5b00c934b3964c3
Dennis Iversen <dennis.iversen@gmail.com>
2017-07-21 18:45:55 +0200

    auto commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// From: https://dev.opera.com/articles/drum-sounds-webaudio/
+function Instrument(context, buffer) {
+    this.context = context;
+    this.buffer = buffer;
+}
+
+Instrument.prototype.setup = function () {
+    this.source = this.context.createBufferSource();
+    this.source.buffer = this.buffer;
+    this.source.connect(this.context.destination);
+};
+
+Instrument.prototype.get = function () {
+    this.source = this.context.createBufferSource();
+    this.source.buffer = this.buffer;
+    return this.source;
+};
+
+Instrument.prototype.trigger = function (time) {
+    this.setup();
+    this.source.start(time);
+};
+
+module.exports = Instrument;
